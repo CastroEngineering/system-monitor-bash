e86472ae3741ef8ab45cbb8504bbc8430a0eb725
CastroEngineering <Joao-Marcos20@protonmail.com>
2025-12-26 14:37:59 -0500

    Add conditional choice

--- Argus.sh
+++ Argus.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-CPU=$(top)
+read -p "Choose an attribute (CPU/RAM/Storage/Uptime/Connection/All)" Choice
 
-Echo "CPU status: $CPU"
+
+#CPU=$(top)
+#RAM=$()
+#Storage=$()
+#Uptime=$()
+Connection=$(ping -c 1 192.168.2.14)
+
+if (( Choice == "All" )); then
+
+#echo "CPU status: $CPU"
+#echo "Ram usage: $RAM"
+#echo "Storage capacity: $Storage"
+#echo "Uptime: $Uptime"
+
+echo "Connection test: $Connection"
+fi
