26f6be279836fdc45a0c4f71b5c083fc3f233a5f
CastroEngineering <Joao-Marcos20@protonmail.com>
2025-12-28 19:47:36 -0500

    Add loop logic

--- Argus.sh
+++ Argus.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-exitProgram=false
+exitFunction ()
+{
 
-until exitProgram; do
+local L_exitLoop=true
+while [[ $L_exitLoop == true ]]; do
+
+read -p "Do you wish to continue? [Y/n]" exitChoice
+
+if [ "$exitChoice" == "Y" ]; then
+    echo "Continuing..."
+    return 0
+
+
+elif [ "$exitChoice" == "n" ]; then
+    exitLoop=true
+    echo "Script finished"
+    return 0
+
+else
+    echo "Check your spelling"
+
+fi
+
+done
+
+}
+
+
+exitLoop=false
+until $exitLoop; do
 
 read -p "Choose an attribute (All/CPU/RAM/Storage/Uptime/Connection)" Choice
 
@@ -13,25 +40,25 @@ read -p "Choose an attribute (All/CPU/RAM/Storage/Uptime/Connection)" Choice
 #Uptime=$()
 Connection=$(ping -c 1 192.168.2.14)
 
-if (( Choice == "All" )); then
+if [ "$Choice" == "All" ]; then
 
 #echo "CPU status: $CPU"
 #echo "Ram usage: $RAM"
 #echo "Storage capacity: $Storage"
 #echo "Uptime: $Uptime"
 
-echo "Connection test: $Connection"
+    echo "Connection test: $Connection"
+    exitFunction
 
-elif ((Choice == "CPU")); then
+elif [ "$Choice" == "CPU" ]; then
 
-#echo "CPU status: $CPU"
+    echo "CPU status: $CPU"
+    exitFunction
 
 else 
 
-echo "No choice made, certified you have typed exactly as writen"
+    echo "No choice made, certified you have typed exactly as writen on the menu options"
 
 fi
-
-read -p "Do you wish to continue? [Y/n]"
 
 done
